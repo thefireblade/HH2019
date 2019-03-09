548d881af3c31fa6f93bb299d34dda9e3f3c3283
thefireblade <jhuang6625@bths.edu>
2019-03-09 11:47:46 -0500

    fixing null prices

--- scripts/routes0.py
+++ scripts/routes0.py
@@ -102,7 +102,7 @@ def sortByPrice(arr):
 @app.route('/api/sort/all/price', methods=['GET'])
 def returnSortedByPrice():
     menuItems = getLatestMenu()
-    return jsonify(sortByPrice(menuItems['menuItems']))
+    return menuItems#jsonify(sortByPrice(menuItems['menuItems']))
 @app.route('/api/menu/update', methods=['GET'])
 def updateMongoWithAllMenus():
     getAllMenuItems()
